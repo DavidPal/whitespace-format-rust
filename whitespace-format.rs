5082f8ebb00c119ca85b5085f5e619aa2a6bf7c1
David Pal <davidko.pal@gmail.com>
2024-09-26 19:28:45 -0700

    Learn more code

--- whitespace-format.rs
+++ whitespace-format.rs
@@ -1,7 +1,16 @@
 use std::env;
+use std::fs;
+const FILE_NAME: &str = "README.md";
 
 fn main() {
+    // Print hello world.
     println!("Hello, world!");
+
+    // Print command line arguments.
     let args: Vec<String> = env::args().collect();
     dbg!(args);
+
+    // Print content of a file.
+    let data: Vec<u8> = fs::read(&FILE_NAME).unwrap();
+    dbg!(String::from_utf8_lossy(&data));
 }
