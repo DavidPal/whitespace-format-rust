0ca77262ff36adee02093b4b31124e0ee5849d2a
David Pal <davidko.pal@gmail.com>
2024-11-09 14:59:32 -0800

    Improve command line parameter descriptions

--- src/cli.rs
+++ src/cli.rs
@@ -8,7 +8,8 @@ const UNMATCHABLE_REGEX: &str = "$.";
 pub enum OutputNewLineMarkerMode {
     #[default]
     #[clap(
-        help = "Use new line marker that is the most common in each individual file. If no new line marker is present in the file, Linux '\\n' is used."
+        help = "Use new line marker that is the most common in each individual file. \
+        If no new line marker is present in the file, Linux '\\n' is used."
     )]
     Auto,
 
@@ -49,12 +50,17 @@ pub enum TrivialFileReplacementMode {
 }
 
 #[derive(clap::Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(
+    version,
+    about = "Whitespace formatter and format checker for text files and source code files.",
+    long_about = "Whitespace formatter and format checker for text files and source code files."
+)]
 pub struct CommandLineArguments {
     #[arg(
         long,
         default_value_t = false,
-        help = "Do not format files. Only report which files would be formatted. Exit code is zero if input is formatted correctly. Exit code is non-zero if formatting is required."
+        help = "Do not format files. Only report which files would be formatted. \
+        Exit code is zero if input is formatted correctly. Exit code is non-zero if formatting is required."
     )]
     pub check_only: bool,
 
@@ -65,12 +71,25 @@ pub struct CommandLineArguments {
     )]
     pub follow_symlinks: bool,
 
-    #[arg(long, default_value_t = String::from(UNMATCHABLE_REGEX),
-    help = "Regular expression that specifies which files to exclude. The regular expression is evaluated on the path of each file. The default value is a regular expression that does not match anything.",
-    long_help = "Regular expression that specifies which files to exclude. The regular expression is evaluated on the path of each file. The default value is a regular expression that does not match anything.\n\nExample #1: --exclude=\"(.jpeg|.png)$\" excludes files with '.jpeg' or '.png' extension.\nExample #2: --exclude=\".git/\" excludes all files in the '.git/' directory.")]
+    #[arg(long,
+    default_value_t = String::from(UNMATCHABLE_REGEX),
+    help =
+        "Regular expression that specifies which files to exclude. \
+        The regular expression is evaluated on the path of each file. \
+        The default value is a regular expression that does not match anything.",
+    long_help =
+        "Regular expression that specifies which files to exclude. \
+        The regular expression is evaluated on the path of each file. \
+        The default value is a regular expression that does not match anything.\
+        \n\n\
+        Example #1: --exclude=\"(.jpeg|.png)$\" excludes files with '.jpeg' or '.png' extension.\n\
+        Example #2: --exclude=\".git/\" excludes all files in the '.git/' directory.\
+    ")]
     pub exclude: String,
 
-    #[arg(long, value_enum, default_value_t = OutputNewLineMarkerMode::Auto, help = "New line marker to use.")]
+    #[arg(long, value_enum,
+    default_value_t = OutputNewLineMarkerMode::Auto,
+    help = "New line marker to use.")]
     pub new_line_marker: OutputNewLineMarkerMode,
 
     #[arg(
@@ -83,7 +102,8 @@ pub struct CommandLineArguments {
     #[arg(
         long,
         default_value_t = false,
-        help = "Remove a new line marker from the end of the file if it is present. This option automatically implies the --remove_trailing_empty_lines option."
+        help = "Remove a new line marker from the end of the file if it is present. \
+        This option automatically implies the --remove_trailing_empty_lines option."
     )]
     pub remove_new_line_marker_from_end_of_file: bool,
 
@@ -112,17 +132,29 @@ pub struct CommandLineArguments {
     #[arg(long, value_enum, default_value_t = TrivialFileReplacementMode::Ignore, help = "Replace files of zero length.")]
     pub normalize_empty_files: TrivialFileReplacementMode,
 
-    #[arg(long, value_enum, default_value_t = TrivialFileReplacementMode::Ignore, help = "Replace files consisting of whitespace only.")]
+    #[arg(long,
+    value_enum,
+    default_value_t = TrivialFileReplacementMode::Ignore,
+    help = "Replace files consisting of whitespace only.")]
     pub normalize_whitespace_only_files: TrivialFileReplacementMode,
 
-    #[arg(long, value_enum, default_value_t = NonStandardWhitespaceReplacementMode::Ignore, help = "Replace or remove non-standard whitespace characters '\\v' and '\\f' in each file.")]
+    #[arg(long,
+    value_enum,
+    default_value_t = NonStandardWhitespaceReplacementMode::Ignore,
+    help = "Replace or remove non-standard whitespace characters '\\v' and '\\f' in each file.")]
     pub normalize_non_standard_whitespace: NonStandardWhitespaceReplacementMode,
 
-    #[arg(long, default_value_t = -1, help = "Replace tabs with spaces. The parameter specifies the number of spaces to use. If the parameter is negative, tabs are not replaced.")]
+    #[arg(long,
+    default_value_t = -1,
+    help = "Replace tabs with spaces. \
+    The parameter specifies the number of spaces to use. \
+    If the parameter is negative, tabs are not replaced.")]
     pub replace_tabs_with_spaces: isize,
 
-    /// List of files and/or directories to process.
-    /// Files in directories are discovered recursively.
-    #[arg(num_args = 1.., required = true, value_delimiter = ' ')]
+    #[arg(num_args = 1..,
+    required = true,
+    value_delimiter = ' ',
+    help = "List of files and/or directories to process. \
+    Files in directories are discovered recursively.")]
     pub paths: Vec<PathBuf>,
 }
