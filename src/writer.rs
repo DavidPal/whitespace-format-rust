d35548c177e0d407d48ff18bac54c3cc0033a390
David Pal <davidko.pal@gmail.com>
2024-11-09 17:43:04 -0800

    Add unit tests for writer module

--- src/writer.rs
+++ src/writer.rs
@@ -28,3 +28,29 @@ impl Writer for CountingWriter {
         self.num_bytes += 1;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_counting_writer() {
+        let mut writer = CountingWriter::new();
+        assert_eq!(writer.get_num_bytes_written(), 0);
+        writer.write(42);
+        assert_eq!(writer.get_num_bytes_written(), 1);
+        writer.write(7);
+        assert_eq!(writer.get_num_bytes_written(), 2);
+        writer.write(13);
+        assert_eq!(writer.get_num_bytes_written(), 3);
+    }
+
+    #[test]
+    fn test_vector_writer() {
+        let mut writer: Vec<u8> = Vec::new();
+        writer.write(42);
+        writer.write(7);
+        writer.write(13);
+        assert_eq!(writer, vec![42, 7, 13]);
+    }
+}
