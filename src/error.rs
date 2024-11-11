bce28eb503a7431acaff96d0cc68b858752dc168
David Pal <davidko.pal@gmail.com>
2024-11-10 16:35:28 -0800

    Use colors

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 // Library imports
 use std::process;
+use colored::Colorize;
 
 pub enum Error {
     FileNotFound(String),
@@ -14,29 +15,29 @@ impl Error {
     pub fn to_string(&self) -> String {
         match &self {
             Error::FileNotFound(file_path) => {
-                format!("File not found '{}'", file_path)
+                format!("File not found '{}'.", file_path)
             }
             Error::FailedToReadDirectory(directory_path) => {
-                format!("Failed to read directory '{}'", directory_path)
+                format!("Failed to read directory '{}'.", directory_path)
             }
             Error::FailedToReadDirectoryEntry(directory_path) => {
-                format!("Failed to read an entry in directory '{}'", directory_path)
+                format!("Failed to read an entry in directory '{}'.", directory_path)
             }
             Error::InvalidRegularExpression(regular_expression) => {
-                format!("Failed to read file '{}'", regular_expression)
+                format!("Failed to read file '{}'.", regular_expression)
             }
             Error::CannotReadFile(file_path) => {
-                format!("Cannot open file '{}' for reading", file_path)
+                format!("Cannot open file '{}' for reading.", file_path)
             }
             Error::CannotWriteFile(file_path) => {
-                format!("Cannot open file '{}' for writing", file_path)
+                format!("Cannot open file '{}' for writing.", file_path)
             }
         }
     }
 }
 
 pub fn die(error: Error) -> ! {
-    println!("{}", error.to_string());
+    println!("{}", error.to_string().red().bold());
     process::exit(1);
 }
 
